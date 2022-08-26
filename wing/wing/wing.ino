80f902378f18e038399027c9e3f5f5aed7bd2560
parkero2 <parkero2@stu.otc.school.nz>
2022-08-26 19:53:02 +1200

    added pin definitions

---
+++ wing/wing/wing.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>
+
+//Declare fader inputs
+int GM = A6;
+int fader1 = A15;
+int fader2 = A14;
+int fader3 = A13;
+int fader4 = A12;
+int fader5 = A11;
+
+//Declare fader controls
+int Fdr1Play = 19;
+int Fdr1Pause = 20;
+int Fdr1Bump = 21;
+int Fdr2Play = 18;
+int Fdr2Pause = 17;
+int Fdr2Bump = 16;
+int Fdr3Play = 15;
+int Fdr3Pause = 14;
+int Fdr3Bump = 13;
+int Fdr4Play = 12;
+int Fdr4Pause = 11;
+int Fdr4Bump = 10;
+int Fdr5Play = 9;
+int Fdr5Pause = 8;
+int Fdr5Bump = 7;
+
+//Declare encoders
+int encA = A2;
+int encB = A3;
+int encC = A4;
+int encD = A5;
+int encE = A7;
+int encF = A8;
+int encX = A9;
+int encY = A10;
+
+//Page controls
+int pgUp = 47;
+int pgDn = 46;
+
+void WifiTansmit() {
+
+}
+
+void setup() {
+
+}
+
+void loop() {
+    
+}
